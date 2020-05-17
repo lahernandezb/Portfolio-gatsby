8c6af4db810a1f2f9e0a27997f0d1f622ad57948
Luis Hernandez <luauhern@gmail.com>
2020-05-16 20:27:22 -0400

    feat: add intro component and styles

---
+++ src/components/intro.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Intro = () => (
+  <div className="intro">
+    <p className="intro__greeting">
+      Hi, I'm <span className="intro__name">Luis Hernandez</span>
+    </p>
+    <p className="intro__description">
+      I'm a <span className="intro__title">full stack software developer </span>
+      based in <span className="intro__location">Dayton, OH </span>
+    </p>
+    <button className="intro__cta">get in touch</button>
+  </div>
+);
+
+export default Intro;
