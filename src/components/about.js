ba36345482c1e90f245a3202ae31718aa957a606
Luis Hernandez <luauhern@gmail.com>
2020-05-16 20:35:19 -0400

    feat: add Intro and About components

---
+++ src/components/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => (
+  <div className="about">
+    <h2 className="about__header">About Me</h2>
+    <p className="about__blurb">
+      Tempus iaculis urna, id volutpat lacus laoreet non curabitur gravida arcu
+      ac tortor dignissim convallis aenean et tortor at risus!
+    </p>
+    <h3 className="about__skills">Skills</h3>
+    <ul className="about-skills-list">
+      <li className="about__skills-item"></li>
+    </ul>
+  </div>
+);
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import Intro from '../components/intro';
+import About from '../components/about';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="main-content-wrapper">
-      <p className="greeting">
-        Hello, I'm <span className="greeting__name">Luis Hernandez</span>
-      </p>
-      <p className="description">
-        I'm a software developer based in Dayton, OH
-      </p>
+      <Intro />
+      <About />
     </div>
   </Layout>
 );
